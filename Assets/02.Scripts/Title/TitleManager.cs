c9a1f95eb308d0473e2311ba688490e355d2d5d6
maecho1111@mju.ac.kr <maecho1111@mju.ac.kr>
2024-10-31 23:47:15 +0900

    타이틀 화면 구현 (성공) - 기존 타이틀 화면에 추가적인 디자인을 입혔습니다. - 버튼의 상태에 따른 스프라이트를 구현했습니다.

--- Assets/02.Scripts/Title/TitleManager.cs
+++ Assets/02.Scripts/Title/TitleManager.cs
@@ -3,10 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    // 시작 버튼 클릭 시 호출
     public void OnStartButtonClicked()
     {
-        SceneManager.LoadScene("PlayGame");
+        SceneManager.LoadScene("PlayGame"); // "PlayGame"을 실제 게임 씬 이름으로 변경
+    }
+
+    // 종료 버튼 클릭 시 호출
+    public void OnEndButtonClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
-
-
